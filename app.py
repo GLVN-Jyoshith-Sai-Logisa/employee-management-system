73d67d609388fcdb7405d3046d366319da65514c
LOGISA GLVN JYOSHITH SAI <logisajyoshithsai@gmail.com>
2026-08-22 18:38:59 +0000

    Prepare app for deployment

--- app.py
+++ app.py
@@ -68,8 +68,9 @@ def delete_employee(id):
     db.session.delete(emp)
     db.session.commit()
     return redirect('/employees')
+    
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
